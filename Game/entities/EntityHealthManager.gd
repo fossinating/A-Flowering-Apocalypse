43b042895a41f0c2ec06545b934e28f73bd2acb4
David <mrriptidemc@gmail.com>
2024-03-30 17:57:49 -0400

    add passive regen for player

--- Game/entities/EntityHealthManager.gd
+++ Game/entities/EntityHealthManager.gd
@@ -3,6 +3,13 @@ class_name EntityHealthManager
 
 
 @export var max_health: int
+
+@export_category("Passive Regen")
+@export var passive_regen_enabled := false
+@export var passive_regen_rate: float
+@export var passive_regen_delay: float
+
+var passive_regen_timer: Timer
 var health: float
 
 
@@ -10,6 +17,16 @@ var health: float
 func _ready():
 	Signals.entity_attacked.connect(entity_attacked)
 	health = max_health
+	passive_regen_timer = Timer.new()
+	passive_regen_timer.set_wait_time(passive_regen_delay)
+	passive_regen_timer.one_shot = true
+	add_child(passive_regen_timer)
+
+
+func _process(delta):
+	if passive_regen_enabled and passive_regen_timer.is_stopped():
+		health = min(max_health, health + passive_regen_rate*delta)
+
 
 
 func save_data():
@@ -24,9 +41,8 @@ func load_data(entity_data):
 
 
 func entity_attacked(attacker: Node, attacked: Node, damage: float):
-	print("Entity attacked")
 	if attacked == get_parent():
-		print("parent attacked")
+		passive_regen_timer.start()
 		health -= damage
 		if health <= 0:
 			Signals.entity_killed.emit(attacker, attacked)

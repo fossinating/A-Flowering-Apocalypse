613bcbcf88dd56abeb97bb2a7f529869572783d9
David <mrriptidemc@gmail.com>
2024-04-01 18:34:45 -0400

    zombie death sound

--- Game/sounds/hurt_sounds.gd
+++ Game/sounds/hurt_sounds.gd
@@ -1,8 +1,10 @@
 extends Node3D
 
 
-@export var stream: AudioStream
+@export var hurt_stream: AudioStream
+@export var dead_stream: AudioStream
 @export_enum("Player", "Entity") var bus = "Entity"
+var has_died = false
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,13 +13,15 @@ func _ready():
 
 
 func entity_attacked(_attacker: Node, attacked: Node, _damage: float):
-	if get_parent() == attacked:
+	if get_parent() == attacked and not has_died:
+		var health_manager = get_parent().find_child("EntityHealthManager", false, true)
 		var player := AudioStreamPlayer3D.new()
 		player.bus = bus
-		player.stream = stream
+		player.stream = dead_stream if health_manager and health_manager.health <= 0 else hurt_stream
 		add_child(player)
 		player.global_position = global_position
 		player.play()
 		await player.finished
 		player.queue_free()
+		has_died = health_manager and health_manager.health <= 0
 
